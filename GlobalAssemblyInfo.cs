48ae8d2078eb515901e3732c95c78af09e4a8e33
Margit Robert <marrob50hz@gmail.com>
2021-07-06 19:05:47 +0200

    update

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -1,3 +1,3 @@
 using System.Reflection;
-[assembly: AssemblyVersion("0.0.0.78")]
-[assembly: AssemblyFileVersion("0.0.0.78")]
+[assembly: AssemblyVersion("0.0.0.79")]
+[assembly: AssemblyFileVersion("0.0.0.79")]
